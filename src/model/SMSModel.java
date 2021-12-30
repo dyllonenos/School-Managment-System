4fd20228be53b30cb93358cb695fbb687a6b9e36
dyllonenos <dyllonenos@gmail.com>
2021-12-30 16:15:42 -0700

    Added addStudentCourse() and added student bio when clicked on student

--- src/model/SMSModel.java
+++ src/model/SMSModel.java
@@ -20,7 +20,7 @@ public class SMSModel extends Observable {
 	 * @param age
 	 */
 	public void addStudent(String studentName, int age) {
-		Student curr_student = new Student(studentName, age);
+		Student curr_student = new Student(studentName.toLowerCase(), age);
 		boolean result;
 		if (this.list_of_students.contains(curr_student)) {
 			result = true;
@@ -39,6 +39,7 @@ public class SMSModel extends Observable {
 	 * @param studentName
 	 */
 	public void removeStudent(String studentName) {
+		studentName = studentName.toLowerCase();
 		for (Student currentStudent : list_of_students) {
 			String currName = currentStudent.getName();
 			if (currName.equals(studentName)) {
@@ -74,6 +75,7 @@ public class SMSModel extends Observable {
 	 * @return
 	 */
 	public HashMap<String, Character> getStudentCourseGrades(String studentName) {
+		studentName = studentName.toLowerCase();
 		for (Student currentStudent : list_of_students) {
 			if (currentStudent.getName().equals(studentName)) {
 				return currentStudent.getAllGrades();
@@ -90,6 +92,7 @@ public class SMSModel extends Observable {
 	 * @return
 	 */
 	public HashMap<String, Integer> getStudentCourseInfo(String studentName) {
+		studentName = studentName.toLowerCase();
 		for (Student currentStudent : list_of_students) {
 			if (currentStudent.getName().equals(studentName)) {
 				return currentStudent.getCourseInformation();
@@ -107,16 +110,45 @@ public class SMSModel extends Observable {
 	 * @return
 	 */
 	public boolean setStudentGrade(String studentName, String courseName, char grade) {
+		studentName = studentName.toLowerCase();
+		courseName = courseName.toLowerCase();
+		
 		for (Student currentStudent : list_of_students) {
+			// If the student exists
 			if (currentStudent.getName().equals(studentName)) {
+				// If the student has taken/is taking the course
 				if (currentStudent.getCourseInformation().containsKey(courseName)) {
 					currentStudent.setCourseGrade(courseName, grade);
 					setChanged();
 					notifyObservers(new SMSMessage("updated", currentStudent, false));
+					return true;
 				}
+				else {
+					// ERROR (Student is not taking this course)
+					setChanged();
+					notifyObservers(new SMSMessage("updated", currentStudent, true));
+					return false;
+				}
+			}
+		}
+		// ERROR (Student is not found)
+		setChanged();
+		notifyObservers(new SMSMessage("updated", null, true));
+		return false;
+	}
+	
+	public boolean addStudentCourse(String studentName, String courseName, int courseUnits) {
+		studentName = studentName.toLowerCase();
+		courseName = courseName.toLowerCase();
+		
+		for (Student currentStudent : list_of_students) {
+			// If the student exists
+			if (currentStudent.getName().equals(studentName)) {
+				currentStudent.addCourse(courseName, courseUnits);
 				return true;
 			}
 		}
+		// ERROR (Student is not found)
 		setChanged();
 		notifyObservers(new SMSMessage("updated", null, true));
 		return false;
--- src/view/SMSGui.java
+++ src/view/SMSGui.java
@@ -1,23 +1,28 @@
 package view;
 
+import java.util.Map;
 import java.util.Observable;
 import java.util.Observer;
-
 import controller.SMSController;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
-import javafx.scene.input.KeyCode;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import model.SMSMessage;
 import model.SMSModel;
@@ -43,15 +48,16 @@ public class SMSGui extends Application implements Observer {
 		main_pane.setCenter(list_pane);
 		Label welcome_text = new Label("Welcome to the School Managment System (SMS)");
 		welcome_text.setUnderline(true);
-		
+
 		Button add_student = setAddButton();
 		Button remove_student = setRemoveButton();
-		
 		Button update_grade = setUpdateButton();
-		
+		Button add_course = setAddCourseButton();
+
 		VBox controls = new VBox();
 		controls.getChildren().add(add_student);
 		controls.getChildren().add(remove_student);
+		controls.getChildren().add(add_course);
 		controls.getChildren().add(update_grade);
 		controls.setPadding(new Insets(5));
 		main_pane.setTop(welcome_text);
@@ -61,49 +67,87 @@ public class SMSGui extends Application implements Observer {
 	/**
 	 * @return
 	 */
-	private Button setUpdateButton() {
-		Button update_grade = new Button("Set grade");
-		update_grade.setOnAction((event)->{
+	private Button setAddCourseButton() {
+		Button add_course = new Button("Add Course");
+		add_course.setOnAction((event) -> {
 			Stage newWindow = new Stage();
 			BorderPane newPane = new BorderPane();
-			
+
+			VBox courses_box = new VBox();
 			TextField name_txt = new TextField();
-			TextField age_txt = new TextField();
-			
-			HBox courses_box = new HBox();
-			TextField course_txt = new TextField();
-			TextField grade_txt = new TextField();
-			courses_box.getChildren().add(course_txt);
-			courses_box.getChildren().add(grade_txt);
-			
-			VBox text_fields = new VBox();
-			text_fields.getChildren().add(name_txt);
-			text_fields.getChildren().add(age_txt);
-			text_fields.getChildren().add(courses_box);
-			
+			name_txt.setFocusTraversable(false);
+			name_txt.setPromptText("Student's name");
+
+			TextField course_name_txt = new TextField();
+			course_name_txt.setFocusTraversable(false);
+			course_name_txt.setPromptText("Course name");
+
+			TextField course_units_txt = new TextField();
+			course_units_txt.setFocusTraversable(false);
+			course_units_txt.setPromptText("Course units");
+
+			courses_box.getChildren().addAll(name_txt, course_name_txt, course_units_txt);
+
+			newPane.setCenter(courses_box);
+
 			Button submit_button = new Button("Submit");
-			
+
+			submit_button.setOnAction((event2) -> {
+				controller.addStudentCourse(name_txt.getText(), course_name_txt.getText(),
+						Integer.valueOf(course_units_txt.getText()));
+				newWindow.close();
+			});
+			newPane.setBottom(submit_button);
+			Scene newScene = new Scene(newPane, 300, 100);
+			newWindow.setTitle("Add course");
+			newWindow.setScene(newScene);
+			newWindow.show();
+		});
+		return add_course;
+	}
+
+	/**
+	 * @return
+	 */
+	private Button setUpdateButton() {
+		Button update_grade = new Button("Set grade");
+		update_grade.setOnAction((event) -> {
+			Stage newWindow = new Stage();
+			BorderPane newPane = new BorderPane();
+
+			TextField name_txt = new TextField();
 			name_txt.setFocusTraversable(false);
 			name_txt.setPromptText("student's name");
-			age_txt.setFocusTraversable(false);
-			age_txt.setPromptText("student's age");
+
+			HBox courses_box = new HBox();
+			TextField course_txt = new TextField();
 			course_txt.setFocusTraversable(false);
 			course_txt.setPromptText("student's course");
-			grade_txt.setFocusTraversable(false);
-			grade_txt.setPromptText("student's course grade");
-			
-			submit_button.setOnAction((event2)->{
+
+			ChoiceBox<Character> grade = new ChoiceBox<>();
+			grade.getItems().addAll('A', 'B', 'C', 'D', 'E', 'I');
+
+			HBox grades = new HBox();
+			grades.getChildren().addAll(new Label("Grade "), grade);
+
+			courses_box.getChildren().addAll(course_txt, grades);
+
+			VBox text_fields = new VBox();
+			text_fields.getChildren().addAll(name_txt, courses_box);
+
+			Button submit_button = new Button("Submit");
+			submit_button.setOnAction((event2) -> {
 				String name = name_txt.getText().trim();
 				String course = course_txt.getText().trim();
-				char grade = Character.valueOf(grade_txt.getText().trim().charAt(0));
-				controller.setStudentGrade(name, course, grade);
+				char grade_letter = grade.getValue();
+				controller.setStudentGrade(name, course, grade_letter);
 				newWindow.close();
 			});
 			newPane.setBottom(submit_button);
 			newPane.setCenter(text_fields);
-			
+
 			Scene newScene = new Scene(newPane, 300, 100);
-			newWindow.setTitle("Add a Student");
+			newWindow.setTitle("Set student's grade");
 			newWindow.setScene(newScene);
 			newWindow.show();
 		});
@@ -114,26 +158,25 @@ public class SMSGui extends Application implements Observer {
 	 * @return
 	 */
 	private Button setAddButton() {
-		Button add_student = new Button("Add");
-		add_student.setOnMouseClicked((event)->{
+		Button add_student = new Button("Add student");
+		add_student.setOnMouseClicked((event) -> {
 			Stage newWindow = new Stage();
 			BorderPane newPane = new BorderPane();
-			
+
 			TextField name_txt = new TextField();
 			TextField age_txt = new TextField();
-			
+
 			VBox text_fields = new VBox();
-			text_fields.getChildren().add(name_txt);
-			text_fields.getChildren().add(age_txt);
-			
+			text_fields.getChildren().addAll(name_txt, age_txt);
+
 			Button submit_button = new Button("Submit");
-			
+
 			name_txt.setFocusTraversable(false);
 			name_txt.setPromptText("student's name");
 			age_txt.setFocusTraversable(false);
 			age_txt.setPromptText("student's age");
-			
-			submit_button.setOnAction((event2)->{
+
+			submit_button.setOnAction((event2) -> {
 				String name = name_txt.getText().trim();
 				int age = Integer.valueOf(age_txt.getText().trim());
 				controller.addStudent(name, age);
@@ -141,12 +184,12 @@ public class SMSGui extends Application implements Observer {
 			});
 			newPane.setBottom(submit_button);
 			newPane.setCenter(text_fields);
-			
+
 			Scene newScene = new Scene(newPane, 300, 100);
 			newWindow.setTitle("Add a Student");
 			newWindow.setScene(newScene);
 			newWindow.show();
-			
+
 		});
 		return add_student;
 	}
@@ -155,23 +198,23 @@ public class SMSGui extends Application implements Observer {
 	 * @return
 	 */
 	private Button setRemoveButton() {
-		Button remove_student = new Button("Remove");
-		remove_student.setOnMouseClicked((event)->{
+		Button remove_student = new Button("Remove student");
+		remove_student.setOnMouseClicked((event) -> {
 			Stage newWindow = new Stage();
 			BorderPane newPane = new BorderPane();
-			
+
 			TextField txt_fld = new TextField();
 			Button submit_button = new Button("Submit");
 			txt_fld.setFocusTraversable(false);
 			txt_fld.setPromptText("Student's name");
-			submit_button.setOnAction((event2)->{
+			submit_button.setOnAction((event2) -> {
 				String name = txt_fld.getText().trim();
 				controller.removeStudent(name);
 				newWindow.close();
 			});
 			newPane.setBottom(submit_button);
 			newPane.setCenter(txt_fld);
-			
+
 			Scene newScene = new Scene(newPane, 300, 100);
 			newWindow.setTitle("Remove a Student");
 			newWindow.setScene(newScene);
@@ -187,58 +230,95 @@ public class SMSGui extends Application implements Observer {
 		Student student = message.getStudent();
 		boolean isError = message.getIsError();
 		int row_count = this.list_pane.getRowCount();
-		
+
 		if (isError == true) {
 			// Alerts
 			String str_message = "Error";
 			if (action.equals("add")) {
 				str_message = "You can not add, this student already exists!";
 			}
-			
+
 			else if (action.equals("remove")) {
 				str_message = "You can not remove a student that does not exist!";
 			}
-			
+
+			else if (action.equals("updated")) {
+				// Student is not found
+				if (student == null) {
+					str_message = "This student does not exist. Add this student first";
+				}
+				// Student it not taking this course
+				else {
+					str_message = "This student is not taking this course. Add this course first";
+				}
+			}
+
 			Alert alert = new Alert(AlertType.ERROR);
 			alert.setTitle("ERROR");
 			alert.setHeaderText("ERROR");
 			alert.setContentText(str_message);
 			alert.show();
 		}
-		
+
 		else {
 			if (action.equals("add")) {
-				HBox curr_hbox = new HBox();
-				curr_hbox.getChildren().add(new Label(student.getName()));
-				curr_hbox.getChildren().add(new Label(" - "));
-				curr_hbox.getChildren().add(new Label("" + student.getAge()));
-				curr_hbox.getChildren().add(new Label(" - "));
-				curr_hbox.getChildren().add(new Label("" + student.getGPA()));
-				this.list_pane.add(curr_hbox, 0, row_count+1);
+				Button student_button = new Button(student.getName());
+				student_button.setBackground(
+						new Background(new BackgroundFill(Color.TRANSPARENT, CornerRadii.EMPTY, Insets.EMPTY)));
+				student_button.setOnAction((event) -> {
+					Stage newWindow = new Stage();
+					BorderPane newPane = new BorderPane();
+
+					VBox student_info = new VBox();
+					student_info.getChildren().addAll(new Label("Name: " + student.getName()),
+							new Label("Age: " + student.getAge()), new Label("GPA: " + student.getGPA()),
+							new Label("SID: " + student.getID()),
+							new Label("Current Units: " + student.getCurrentUnits()));
+					newPane.setTop(student_info);
+
+					ScrollPane scroll = new ScrollPane();
+					scroll.setPadding(new Insets(5));
+
+					GridPane course_list = new GridPane();
+					scroll.setContent(course_list);
+
+					for (Map.Entry<String, Integer> entry : student.getCourseInformation().entrySet()) {
+						String course_name = entry.getKey();
+						int course_units = entry.getValue();
+						char course_grade = student.getAllGrades().get(course_name);
+						String text = "Course: " + course_name + ", Units: " + course_units + ", Grade: "
+								+ course_grade;
+						course_list.add(new Label(text), 0, course_list.getRowCount() + 1);
+					}
+
+					newPane.setCenter(scroll);
+
+					Scene newScene = new Scene(newPane, 300, 300);
+					newWindow.setTitle("Student Info");
+					newWindow.setScene(newScene);
+					newWindow.show();
+				});
+				this.list_pane.add(student_button, 0, row_count + 1);
 			}
-			
+
 			else if (action.equals("remove")) {
 				for (Node curr_node : this.list_pane.getChildren()) {
-					HBox curr_hbox = (HBox) curr_node;
-					
-					Label name_label = (Label) curr_hbox.getChildren().get(0);
-					Label age_label = (Label) curr_hbox.getChildren().get(2);
-					
-					String curr_name = name_label.getText();
-					int curr_age = Integer.valueOf(age_label.getText());
-					
-					if (curr_name.equals(student.getName()) && curr_age == student.getAge()) {
+					Button curr_hbox = (Button) curr_node;
+
+					String curr_name = curr_hbox.getText();
+
+					if (curr_name.equals(student.getName())) {
 						this.list_pane.getChildren().remove(curr_node);
 						break;
 					}
 				}
 			}
-			
+
 			else if (action.equals("updated")) {
-				
+
 			}
 		}
-		
+
 	}
-	
+
 }
